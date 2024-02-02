3f8288a48e246c82cc1fc6f0777921b3a9a3d7e6
kyo chan <kyochan@torstar.ca>
2024-02-02 14:37:01 -0500

    init

--- components/Search.tsx
+++ components/Search.tsx
@@ -5,12 +5,15 @@ import { useCallback, useRef, useState } from 'react';
 
 import rssParser from './RssParser';
 
-const copyToClipboard = (value: string) => {
+const copyToClipboard = (value: string, id: any) => {
   navigator.clipboard.writeText(value);
+  console.log('>>>>>>>> id, value', id, value)
+  document.getElementById(id).style.display = "inline-block";
+  setTimeout(() => document.getElementById(id).style.display = "none", 1000)
 };
 
 const DataRow = ({ data }: any) => {
-  const { label, labelValue, value } = data;
+  const { label, labelValue, value, i } = data;
 
   if (value === undefined || value === '') {
     return <></>;
@@ -27,7 +30,7 @@ const DataRow = ({ data }: any) => {
             <span>{label}</span>
             <button
               type="button"
-              onClick={() => copyToClipboard(labelValue)}
+              onClick={(e) => copyToClipboard(labelValue, `copy-title-${labelValue}-${i}`)}
               className="js-clipboard ml-2 inline-flex items-center gap-x-2 rounded-lg border border-gray-200 bg-white p-1 text-sm font-medium text-gray-800 shadow-sm hover:bg-gray-50 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-700 dark:bg-slate-900 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
               data-clipboard-target="#hs-clipboard-basic"
               data-clipboard-action="copy"
@@ -63,13 +66,14 @@ const DataRow = ({ data }: any) => {
               >
                 <polyline points="20 6 9 17 4 12" />
               </svg>
+              <span className="text-xs" style={{display: 'none', color: 'red'}} id={`copy-title-${labelValue}-${i}`}>Copied</span>
             </button>
           </div>
           <div className="text-gray-800">
             <span>{value}</span>
             <button
               type="button"
-              onClick={() => copyToClipboard(value)}
+              onClick={(e) => copyToClipboard(value, `copy-value-${labelValue}-${i}`)}
               className="js-clipboard ml-2 inline-flex items-center gap-x-2 rounded-lg border border-gray-200 bg-white p-1 text-sm font-medium text-gray-800 shadow-sm hover:bg-gray-50 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-700 dark:bg-slate-900 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
               data-clipboard-target="#hs-clipboard-basic"
               data-clipboard-action="copy"
@@ -105,6 +109,7 @@ const DataRow = ({ data }: any) => {
               >
                 <polyline points="20 6 9 17 4 12" />
               </svg>
+              <span className="text-xs" style={{display: 'none', color: 'red'}} id={`copy-value-${labelValue}-${i}`}>Copied</span>
             </button>
           </div>
         </div>
@@ -176,6 +181,7 @@ export default function Search() {
   const selectRef = useRef<any>('');
   const [cache, setCache] = useState<any>([]);
   const [result, setResult] = useState<any>([]);
+  const [loading, setLoading] = useState(false)
 
   const searchFromCache = (data: any) => {
     const text = (inputRef.current?.value ?? '').toLowerCase();
@@ -193,9 +199,12 @@ export default function Search() {
         return;
       }
 
-      searchFromCache(cache);
+      setLoading(true)
+      const r = searchFromCache(cache);
+      if (r.length > 0) {
+        setLoading(false)
+      }
 
-      // background update
       const { data } = await axios.get(
         `${rssBaseUrl}/${selectRef.current?.value}`,
       );
@@ -204,6 +213,9 @@ export default function Search() {
       searchFromCache(podcastData);
     } catch (e) {
       console.log('>>>>>>> err', e);
+      setResult([]);
+    } finally {
+      setLoading(false)
     }
   }, [cache]);
 
@@ -224,6 +236,7 @@ export default function Search() {
           <select
             ref={selectRef}
             required
+            onChange={() => setResult([])}
             className="focus:shadow-outline block w-full appearance-none rounded border border-gray-400 bg-white px-4 py-2 pr-8 leading-tight shadow hover:border-gray-500 focus:outline-none"
           >
             <option>Select a show</option>
@@ -275,15 +288,29 @@ export default function Search() {
         </div>
       </div>
       <div className="w-full">
-        {result?.map((r: any, i: number) => {
+        {loading && (
+          <div role="status" className="flex w-full text-center items-center pt-10">
+            <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                 viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                fill="currentColor"/>
+              <path
+                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                fill="currentFill"/>
+            </svg>
+          </div>
+        )}
+        {!loading && result?.map((r: any, i: number) => {
           return (
             <div key={i} className="text-m my-2 w-full overflow-hidden rounded border-4 border-indigo-200 border-l-indigo-500 border-t-indigo-700 p-2">
-              <h2>{r.title}</h2>
+              <h2 className="text-xl">{r.title}</h2>
               <DataRow
                 data={{
                   label: 'Episode ID',
                   labelValue: 'episode_id',
                   value: r.guid,
+                  i
                 }}
               />
               <DataRow
@@ -291,6 +318,7 @@ export default function Search() {
                   label: 'Episode Audio Url',
                   labelValue: 'episode_audio_url',
                   value: r.enclosures?.[0]?.url,
+                  i
                 }}
               />
               <DataRow
@@ -298,6 +326,7 @@ export default function Search() {
                   label: 'Episode Duration',
                   labelValue: 'episode_duration',
                   value: r.duration,
+                  i
                 }}
               />
               <DataRow
@@ -305,6 +334,7 @@ export default function Search() {
                   label: 'Episode Number',
                   labelValue: 'episode_no',
                   value: r.episode,
+                  i
                 }}
               />
               <DataRow
@@ -312,6 +342,7 @@ export default function Search() {
                   label: 'Episode Season',
                   labelValue: 'episode_season',
                   value: r.season,
+                  i
                 }}
               />
             </div>
--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,7 @@ const nextConfig = {
    *
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
-  basePath: "/libsyn-podcast-search",
+  basePath: process.env.ENV === "local" ? "" : "/libsyn-podcast-search",
 
   /**
    * Disable server-based image optimization. Next.js does not support
