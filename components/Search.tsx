2f9be50f2358e9a6e238d85a913e19e546c31d30
kyo chan <kyochan@torstar.ca>
2024-02-02 14:55:51 -0500

    init

--- components/Search.tsx
+++ components/Search.tsx
@@ -263,7 +263,7 @@ export default function Search() {
             onKeyUp={(e) => pressEnterOnSearchInput(e)}
             ref={inputRef}
             className="rounded-s-gray-100 rounded-s-2 z-20 block w-full rounded-e-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400 dark:focus:border-blue-500"
-            placeholder="Search"
+            placeholder="Search with blox article title (partial match)"
             required
           />
           <button
